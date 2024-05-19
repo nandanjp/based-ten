34f866accffe11bbe1d7f6a77f31ad2f9b3faa7b
Nandan Patel <nandan.jp17@gmail.com>
2024-05-18 23:30:55 -0400

    initial commit, setup simple express server and prisma, added a server check endpoint

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# data-based
+# Based-Data (Another Title Go Here)
+
+## Group Members
+
+# An Application About Based Data
+
 a based data intensive application (???)
+
+## Brief Description About Application
+
+## Figure Out What Tech Stack to Describe
+
+## Speak About Front End
+
+## Speak About Back End
+
+### Usage
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import "module-alias/register";
+
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import express, { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(`/api/health`, (_req: Request, res: Response) =>
+{
+    console.log("Server is Healthy");
+    res.status(StatusCodes.OK).json({
+        status: StatusCodes.OK,
+        message: "server is healthy and running!"
+    });
+});
+
+app.listen(process.env.PORT, () =>
+{
+    console.log(`Now listening on port: ${process.env.PORT}`);
+});
