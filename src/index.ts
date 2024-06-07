c9672b7a8120557c52dfbe3dd59071bde202a3ad
Nandan Patel <nandan.jp17@gmail.com>
2024-06-07 15:49:33 -0400

    removed prisma and switching express out for featherjs

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import helmet from "helmet";
 import morgan from "morgan";
 import express, { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import prisma from "../lib/db";
 
 const app = express();
 
@@ -23,43 +22,31 @@ app.use(`/api/health`, (_req: Request, res: Response) => {
 });
 
 app.get(`/api/manga`, async (_req: Request, res: Response) => {
-  const manga = await prisma.manga.findMany();
-  if (!manga) {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      status: StatusCodes.BAD_REQUEST,
-      manga,
-      message: "Failed to retrieve all manga currently in the database",
-    });
-    return;
-  }
   res.status(StatusCodes.OK).json({
     status: StatusCodes.OK,
-    manga,
+    message: "Received all manga currently in the database",
+  });
+});
+
+app.get(`/api/manga`, async (_req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({
+    status: StatusCodes.OK,
     message: "Received all manga currently in the database",
   });
 });
 
 app.get(`/api/anime`, async (_req: Request, res: Response) => {
-  const anime = await prisma.anime.findMany();
-  if (!anime) {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      status: StatusCodes.BAD_REQUEST,
-      anime,
-      message: "Failed to retrieve all manga currently in the database",
-    });
-    return;
-  }
   res.status(StatusCodes.OK).json({
     status: StatusCodes.OK,
-    anime,
-    message: "Received all manga currently in the database",
+    message: "Received all anime currently in the database",
   });
 });
 
 app.use("/api/videogames", (_req: Request, res: Response) => {
-  console.log("Requesting all video games");
-  const allVideoGames = prisma.videoGame.findMany();
-  res.send(allVideoGames);
+  res.status(StatusCodes.OK).json({
+    status: StatusCodes.OK,
+    message: "Received all video games currently in the database",
+  });
 });
 
 app.listen(process.env.PORT, () => {
