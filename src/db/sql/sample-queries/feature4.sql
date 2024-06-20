0b36ff2efe417b3a7ab5a43dea39f0729d949ae7
Nandan Patel <nandan.jp17@gmail.com>
2024-06-20 18:50:02 -0400

    format

--- src/db/sql/sample-queries/feature4.sql
+++ src/db/sql/sample-queries/feature4.sql
@@ -1,13 +1,14 @@
 WITH mutuals AS (
     SELECT f1.followingEmail
-    FROM Follows f1, Follows f2
-    WHERE f1.followerEmail = jane.smith@example.net
-    AND f1.followingEmail = f2.followerEmail 
-    AND f2.followingEmail = jane.smith@example.net
+    FROM Follows f1,
+        Follows f2
+    WHERE f1.followerEmail = jane.smith @example.net
+        AND f1.followingEmail = f2.followerEmail
+        AND f2.followingEmail = jane.smith @example.net
 )
 SELECT followingEmail,
-CASE 
-    WHEN followingEmail IN mutuals THEN TRUE
-    ELSE FALSE
-FROM Follows
-WHERE followerEmail = jane.smith@example.net;
+    CASE
+        WHEN followingEmail IN mutuals THEN TRUE
+        ELSE FALSE
+        FROM Follows
+        WHERE followerEmail = jane.smith @example.net;
