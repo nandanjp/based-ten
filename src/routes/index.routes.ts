cdbdcc237051c39e894d864d5b232a63a49f0b38
Nandan Patel <nandan.jp17@gmail.com>
2024-06-20 23:25:05 -0400

    entered api endpoints to complete

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -13,3 +13,47 @@ router.use('/api/user', user_router)
 router.use('/api/video-game', games_router)
 
 export default router
+
+/*
+Users:
+- get all users
+- get a user by email, displayName
+- get user's lists
+- create a user
+- update user email, displayName, or password
+- delete user
+- create a new list
+- add items to a list
+- update an item in the list
+
+
+Followers:
+- get list of followers for a given user
+- get list of users that a user is following
+- follow a user
+- unfollow a user
+
+Likes:
+- like a list
+- remove liked list
+- get all liked lists of a user
+- get top liked lists
+- get all media show
+- get most liked media from all lists in a group
+
+Movies, Songs, VideoGames, Anime:
+- get all media
+- get media by id
+- create new media
+- update existing media
+- delete existing media
+
+Groups:
+- create a new group
+- add a user to a group
+- get all members in a group
+- get all lists in a group
+- update group name
+- delete an existing group
+- remove a user from a group
+*/
