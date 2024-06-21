dc6790444ac9f2fd4a9e5151d19484aaf6d7a66d
Jessie <jessie56789@gmail.com>
2024-06-20 22:00:44 -0400

    added back Media view in create-tables

--- src/db/sql/create-tables.sql
+++ src/db/sql/create-tables.sql
@@ -71,3 +71,31 @@ CREATE TABLE Follows (
     followingEmail VARCHAR(30) NOT NULL REFERENCES Users(email),
     PRIMARY KEY (followerEmail, followingEmail)
 );
+CREATE VIEW Media AS
+SELECT id,
+    title,
+    mediaImage,
+    createdOn,
+    'movie' AS type
+FROM Movies
+UNION
+SELECT id,
+    title,
+    mediaImage,
+    createdOn,
+    'song' AS type
+FROM Songs
+UNION
+SELECT id,
+    title,
+    mediaImage,
+    createdOn,
+    'videogame' AS type
+FROM VideoGames
+UNION
+SELECT id,
+    title,
+    mediaImage,
+    createdOn,
+    'anime' AS type
+FROM Anime;
