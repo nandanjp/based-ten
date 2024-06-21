78fadb65f795499ae42f50ca39dd72af5e6bee2d
Nandan Patel <nandan.jp17@gmail.com>
2024-06-20 23:57:48 -0400

    refactoring the insert script, going to add endpoints next

--- src/db/seed/insert.ts
+++ src/db/seed/insert.ts
@@ -15,93 +15,141 @@ import {
 } from './data'
 import { z } from 'zod'
 import { QueryConfig } from 'pg'
+import {
+  AnimeFilesType,
+  FollowsFileType,
+  GroupMembersFileType,
+  GroupsFileType,
+  LikesFileType,
+  ListItemsFileType,
+  ListsFileType,
+  MoviesFileType,
+  SongsFileType,
+  UsersFileType,
+  VideoGamesFileType
+} from './data-schemas'
 
-const insertAnime = async () => {
-  const seed_data = await SeedAnime()
-  await insert(seed_data, (anime) => ({
-    text: 'INSERT INTO Anime(title, mediaImage, numEpisodes, createdOn) VALUES($1, $2, $3, $4) RETURNING id, title, mediaImage, numEpisodes;',
-    values: [anime.title, anime.image, anime.episodes, anime.released_on]
-  }))
-}
+type Tables =
+  | 'anime'
+  | 'games'
+  | 'movies'
+  | 'songs'
+  | 'users'
+  | 'groups'
+  | 'group_members'
+  | 'lists'
+  | 'list_items'
+  | 'likes'
+  | 'follows'
 
-const insertGames = async () => {
-  const seed_data = await SeedGames()
-  await insert(seed_data, (game) => ({
-    text: 'INSERT INTO VideoGames(title, mediaImage, console, createdOn) VALUES($1, $2, $3, $4) RETURNING id, title, mediaImage, console;',
-    values: [game.title, game.image, game.platform, game.released_on]
-  }))
-}
+const insert_anime = (anime: AnimeFilesType) => ({
+  text: 'INSERT INTO Anime(title, mediaImage, numEpisodes, createdOn) VALUES($1, $2, $3, $4) RETURNING id, title, mediaImage, numEpisodes;',
+  values: [anime.title, anime.image, anime.episodes, anime.released_on]
+})
+const insert_game = (game: VideoGamesFileType) => ({
+  text: 'INSERT INTO VideoGames(title, mediaImage, console, createdOn) VALUES($1, $2, $3, $4) RETURNING id, title, mediaImage, console;',
+  values: [game.title, game.image, game.platform, game.released_on]
+})
+const insert_movie = (movie: MoviesFileType) => ({
+  text: 'INSERT INTO Movies(title, mediaImage, createdOn) VALUES($1, $2, $3) RETURNING id, title, mediaImage;',
+  values: [movie.title, movie.image, movie.released_on]
+})
+const insert_song = (song: SongsFileType) => ({
+  text: 'INSERT INTO Songs(title, author, album, mediaImage, createdOn) VALUES($1, $2, $3, $4, $5) RETURNING id, author, album, title, mediaImage;',
+  values: [song.title, song.writer, song.album, song.image, song.released_in_year]
+})
+const insert_user = (user: UsersFileType) => ({
+  text: 'INSERT INTO Users(email, displayName, userPassword) VALUES($1, $2, $3) RETURNING email, displayName, userPassword;',
+  values: [user.user_email, user.user_name, user.password]
+})
+const insert_group = (group: GroupsFileType) => ({
+  text: 'INSERT INTO Groups(groupName,ownedBy) VALUES($1, $2) RETURNING gid, groupName, ownedBy;',
+  values: [group.name, group.user_email]
+})
+const insert_group_member = (group_member: GroupMembersFileType) => ({
+  text: 'INSERT INTO GroupMembers(gid, email) VALUES($1, $2) RETURNING gid, email;',
+  values: [group_member.group_id, group_member.member_email]
+})
+const insert_like = (like: LikesFileType) => ({
+  text: 'INSERT INTO Likes(likerEmail, likingEmail, listName) VALUES($1, $2, $3) RETURNING likerEmail, likingEmail, listName;',
+  values: [like.liker_email, like.liking_email, like.list_name]
+})
+const insert_list = (list: ListsFileType) => ({
+  text: 'INSERT INTO Lists(email, listName, listType) VALUES($1, $2, $3) RETURNING email, listName, listType;',
+  values: [list.user_email, list.list_name, list.list_type]
+})
+const insert_list_item = (insert_list_item: ListItemsFileType) => ({
+  text: 'INSERT INTO ListItems(email, listName, itemID, rankingInList) VALUES($1, $2, $3, $4) RETURNING email, listName, itemID, rankingInList;',
+  values: [
+    insert_list_item.user_email,
+    insert_list_item.list_name,
+    insert_list_item.item_id,
+    insert_list_item.rank_in_list
+  ]
+})
+const insert_follow = (follow: FollowsFileType) => ({
+  text: 'INSERT INTO Follows(followerEmail, followingEmail) VALUES($1, $2) RETURNING followerEmail, followingEmail;',
+  values: [follow.follower_email, follow.following_email]
+})
 
-const insertMovies = async () => {
-  const seed_data = await SeedMovies()
-  await insert(seed_data, (movie) => ({
-    text: 'INSERT INTO Movies(title, mediaImage, createdOn) VALUES($1, $2, $3) RETURNING id, title, mediaImage;',
-    values: [movie.title, movie.image, movie.released_on]
-  }))
-}
-
-const insertSongs = async () => {
-  const seed_data = await SeedSongs()
-  await insert(seed_data, (song) => ({
-    text: 'INSERT INTO Songs(title, author, album, mediaImage, createdOn) VALUES($1, $2, $3, $4, $5) RETURNING id, author, album, title, mediaImage;',
-    values: [song.title, song.writer, song.album, song.image, song.released_in_year]
-  }))
-}
-
-const insertUsers = async () => {
-  const seed_data = await SeedUsers()
-  await insert(seed_data, (user) => ({
-    text: 'INSERT INTO Users(email, displayName, userPassword) VALUES($1, $2, $3) RETURNING email, displayName, userPassword;',
-    values: [user.user_email, user.user_name, user.password]
-  }))
-}
-
-const insertGroups = async () => {
-  const seed_data = await SeedGroups()
-  await insert(seed_data, (group) => ({
-    text: 'INSERT INTO Groups(groupName,ownedBy) VALUES($1, $2) RETURNING gid, groupName, ownedBy;',
-    values: [group.name, group.user_email]
-  }))
-}
-
-const insertGroupMembers = async () => {
-  const seed_data = await SeedGroupMembers()
-  await insert(seed_data, (group_member) => ({
-    text: 'INSERT INTO GroupMembers(gid, email) VALUES($1, $2) RETURNING gid, email;',
-    values: [group_member.group_id, group_member.member_email]
-  }))
-}
-
-const insertLikes = async () => {
-  const seed_data = await SeedLikes()
-  await insert(seed_data, (like) => ({
-    text: 'INSERT INTO Likes(likerEmail, likingEmail, listName) VALUES($1, $2, $3) RETURNING likerEmail, likingEmail, listName;',
-    values: [like.liker_email, like.liking_email, like.list_name]
-  }))
-}
-
-const insertLists = async () => {
-  const seed_data = await SeedLists()
-  await insert(seed_data, (list) => ({
-    text: 'INSERT INTO Lists(email, listName, listType) VALUES($1, $2, $3) RETURNING email, listName, listType;',
-    values: [list.user_email, list.list_name, list.list_type]
-  }))
-}
-
-const insertListItems = async () => {
-  const seed_data = await SeedListItems()
-  await insert(seed_data, (list) => ({
-    text: 'INSERT INTO ListItems(email, listName, itemID, rankingInList) VALUES($1, $2, $3, $4) RETURNING email, listName, itemID, rankingInList;',
-    values: [list.user_email, list.list_name, list.item_id, list.rank_in_list]
-  }))
-}
-
-const insertFollows = async () => {
-  const seed_data = await SeedFollows()
-  await insert(seed_data, (follow) => ({
-    text: 'INSERT INTO Follows(followerEmail, followingEmail) VALUES($1, $2) RETURNING followerEmail, followingEmail;',
-    values: [follow.follower_email, follow.following_email]
-  }))
+const insertToTable = async (table: Tables) => {
+  switch (table) {
+    case 'anime': {
+      const seed_data = await SeedAnime()
+      await insert(seed_data, insert_anime)
+      break
+    }
+    case 'follows': {
+      const seed_data = await SeedFollows()
+      await insert(seed_data, insert_follow)
+      break
+    }
+    case 'games': {
+      const seed_data = await SeedGames()
+      await insert(seed_data, insert_game)
+      break
+    }
+    case 'group_members': {
+      const seed_data = await SeedGroupMembers()
+      await insert(seed_data, insert_group_member)
+      break
+    }
+    case 'groups': {
+      const seed_data = await SeedGroups()
+      await insert(seed_data, insert_group)
+      break
+    }
+    case 'likes': {
+      const seed_data = await SeedLikes()
+      await insert(seed_data, insert_like)
+      break
+    }
+    case 'list_items': {
+      const seed_data = await SeedListItems()
+      await insert(seed_data, insert_list_item)
+      break
+    }
+    case 'lists': {
+      const seed_data = await SeedLists()
+      await insert(seed_data, insert_list)
+      break
+    }
+    case 'movies': {
+      const seed_data = await SeedMovies()
+      await insert(seed_data, insert_movie)
+      break
+    }
+    case 'songs': {
+      const seed_data = await SeedSongs()
+      await insert(seed_data, insert_song)
+      break
+    }
+    case 'users': {
+      const seed_data = await SeedUsers()
+      await insert(seed_data, insert_user)
+      break
+    }
+  }
 }
 
 async function insert<T>(values: Array<T>, map_fn: (t: T) => QueryConfig) {
@@ -147,54 +195,50 @@ async function main() {
   await client.connect()
   switch (process.argv[2] as z.infer<typeof ProvidedCorrectInsert>) {
     case 'anime':
-      await insertAnime()
+      await insertToTable('anime')
       break
     case 'games':
-      await insertGames()
+      await insertToTable('games')
       break
     case 'movies':
-      await insertMovies()
+      await insertToTable('movies')
       break
     case 'songs':
-      await insertSongs()
+      await insertToTable('songs')
       break
     case 'users':
-      await insertUsers()
+      await insertToTable('users')
       break
     case 'groups':
-      await insertGroups()
+      await insertToTable('groups')
       break
     case 'group-members':
-      await insertGroupMembers()
+      await insertToTable('group_members')
       break
     case 'likes':
-      await insertLikes()
+      await insertToTable('likes')
       break
     case 'lists':
-      await insertLists()
+      await insertToTable('lists')
       break
     case 'list-items':
-      await insertListItems()
+      await insertToTable('list_items')
       break
     case 'follows':
-      await insertFollows()
+      await insertToTable('follows')
       break
     case 'all':
-      await insertAnime()
-      await insertGames()
-      await insertMovies()
-      await insertSongs()
-      await insertUsers()
-      await insertGroups()
-      await insertGroupMembers()
-      await insertLists()
-      await insertLikes()
-      await insertListItems()
-      break
-    default:
-      console.error(
-        `This should have been flagged as you did not provided a correct insert table name: ${process.argv[2]}`
-      )
+      await insertToTable('anime')
+      await insertToTable('games')
+      await insertToTable('movies')
+      await insertToTable('songs')
+      await insertToTable('users')
+      await insertToTable('groups')
+      await insertToTable('group_members')
+      await insertToTable('lists')
+      await insertToTable('likes')
+      await insertToTable('list_items')
+      await insertToTable('follows')
   }
   client.end()
 }
