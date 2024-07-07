53460b613be81db656dfec1489ec5864d31f5c1c
Jessie <jessie56789@gmail.com>
2024-07-06 23:09:32 -0400

    update feature6

--- src/db/sql/sample-queries/feature6.sql
+++ src/db/sql/sample-queries/feature6.sql
@@ -8,11 +8,6 @@ WITH RECURSIVE Circles AS (
         FROM Circles c JOIN GroupMembers gm1 ON c.gid = gm1.gid
         JOIN GroupMembers gm2 ON gm1.email = gm2.email
         JOIN Groups g ON gm2.gid = g.gid
-        WHERE gm2.email IN (
-            SELECT u.email
-            FROM Users u JOIN GroupMembers gm ON u.email = gm.email
-            WHERE u.email != 'john.doe@example.com'
-        )
     )
 )
 
