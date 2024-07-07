b15ee6d95b7e835afe3906fa87a5b9320d0d03b8
Justin Lin <tojustinlin@gmail.com>
2024-07-06 20:43:02 -0400

    Create feature5

--- src/db/sql/sample-queries/feature5.sql
+++ src/db/sql/sample-queries/feature5.sql
@@ -3,11 +3,45 @@
 -- 3. Find a score for each list based on Similarity and Popularity. Have this score in a column next to List ID
 -- 4. Return the top 10 recommended lists
 
-WITH RecentlyRankedItems AS (
-
+WITH YourRankedItems AS (
+    SELECT li.email, li.itemID
+    FROM ListItems li
+    WHERE li.email = 'example_email'
 ),
-MutualLists AS (
-
+SimilarLists AS (
+    SELECT l.email, l.listName, COUNT(lk.likerEmail) AS likes
+    FROM Lists l JOIN Likes lk ON l.email = lk.likingEmail AND l.listName = lk.listName
+    WHERE EXISTS (
+        SELECT 1
+        FROM ListItems li
+        WHERE li.email = l.email AND li.listName = l.listName AND li.itemID IN (
+            SELECT yri.itemID
+            FROM YourRankedItems yri
+        )
+    )
+    GROUP BY l.email, l.listName
 ),
+SimilarListsWithSimilarity AS (
+    SELECT sl.email, sl.listName, sl.likes, COUNT(li.itemID) AS similarity
+    FROM SimilarLists sl JOIN ListItems li ON sl.email = li.email AND sl.listName = li.listName
+    WHERE li.itemID IN (
+        SELECT yri.itemID
+        FROM YourRankedItems yri
+    )
+    GROUP BY sl.email, sl.listName, sl.likes
+),
+TotalUsers AS (
+    SELECT COUNT(DISTINCT email) AS totalUsers
+    FROM Users
+),
+ListsByScore AS (
+    SELECT sl.email, sl.listName, (sl.similarity / 10) + (sl.likes / tu.totalUsers) AS score
+    FROM SimilarListsWithSimilarity sl, TotalUsers tu
+)
 
-SELECT * FROM MutualLists
+SELECT lbs.email, lbs.listName, lbs.score
+FROM ListsByScore lbs
+ORDER BY lbs.score DESC
+LIMIT 10;
+
+
