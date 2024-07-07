407fe469448369222f0a430a0f8f9fc7aacef00c
Justin Lin <tojustinlin@gmail.com>
2024-07-06 22:17:56 -0400

    Update feature6.sql

--- src/db/sql/sample-queries/feature6.sql
+++ src/db/sql/sample-queries/feature6.sql
@@ -7,7 +7,8 @@ WITH RECURSIVE Circles AS (
         SELECT c.level + 1, gm.gid, g.groupName, g.ownedBy
         FROM Circles c JOIN GroupMembers gm ON c.gid = gm.gid
         JOIN Groups g ON gm.gid = g.gid
-        WHERE c.level < 3 AND gm.email IN (
+        WHERE c.level < 3 AND gm.gid NOT IN (SELECT gid FROM Circles)
+        AND gm.email IN (
             SELECT u.email
             FROM Users u JOIN GroupMembers gm ON u.email = gm.email
             WHERE gm.gid = 999 AND u.email != 'your_own'
