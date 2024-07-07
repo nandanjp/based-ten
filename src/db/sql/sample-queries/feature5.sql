b073bbb360b93c0c2c0d8ca71dab48f1ad464d80
Justin Lin <tojustinlin@gmail.com>
2024-07-06 20:11:22 -0400

    Initial commit

---
+++ src/db/sql/sample-queries/feature5.sql
@@ -0,0 +1,13 @@
+-- 1. Find lists that have items in common with your recently ranked items
+-- 2. Order lists by most likes
+-- 3. Find a score for each list based on Similarity and Popularity. Have this score in a column next to List ID
+-- 4. Return the top 10 recommended lists
+
+WITH RecentlyRankedItems AS (
+
+),
+MutualLists AS (
+
+),
+
+SELECT * FROM MutualLists
