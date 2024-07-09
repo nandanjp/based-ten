0757066e10f965c86dd870eb906708e38b1a9cf0
Jessie <jessie56789@gmail.com>
2024-07-08 21:56:30 -0400

    added endpoint for mutual followers

---
+++ src/models/followmutual.rs
@@ -0,0 +1,25 @@
+use crate::utils::traits::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize)]
+pub struct FollowMutual {
+    pub following_email: String,
+    pub follows_back: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct FollowMutualError(pub String);
+impl Error for FollowMutualError {
+    fn new(err: String) -> Self {
+        Self(err)
+    }
+}
+impl std::fmt::Display for FollowMutualError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "failed to retrieve mutual follows due to the following error: {:#?}",
+            self.0
+        )
+    }
+}
