b0c53bd9a0064314793151f20a855f1c30695167
Justin Lin <tojustinlin@gmail.com>
2024-07-20 18:02:54 -0400

    Use Next Image instead of <img>

--- client/src/components/blocks/ViewListItem/ViewListItem.tsx
+++ client/src/components/blocks/ViewListItem/ViewListItem.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ViewListItemProps } from "./types";
 
 const ViewListItem = (props: ViewListItemProps) => {
@@ -6,7 +7,9 @@ const ViewListItem = (props: ViewListItemProps) => {
     <div
         className="flex gap-4 justify-normal rounded-2xl px-8 py-10 w-80 bg-primary text-white hover:bg-gray-700"
       >
-        <img src={listItem.media_image} className="w-12 h-12" />
+        <Image width={500}
+      height={500}
+      alt="Picture of the author" src={listItem.media_image} className="w-12 h-12" />
         <div className="flex flex-col gap-2 items-center justify-center">
           <h1 className="font-semibold italic text-base">
             {listItem.title}
