925ecc5af9cbeb0b68e63286c23bed49f1f24375
Jessie <jessie56789@gmail.com>
2024-07-25 18:34:05 -0400

    fixing endpoint

--- src/services/lists.rs
+++ src/services/lists.rs
@@ -217,67 +217,210 @@ impl ListService {
         pool: &sqlx::PgPool,
         list_type: ListType,
     ) -> Result<Vec<List>, ErrorList> {
-        sqlx::query_as!(
-            ListOrderedByLikes,
-            r#"WITH ListsWithItemIDs AS (
-                SELECT l.userName,
-                    l.listName,
-                    li.itemID,
-                    l.listType as itemType
-                FROM ListItems li
-                    JOIN Lists l ON li.userName = l.userName
-                    AND li.listName = l.listName
-            ),
-            ListsWithLikes AS (
-                SELECT l.*
-                FROM Lists l
-                    JOIN Likes lk on l.listName = lk.listName
-            ),
-            ListLikeCounts AS (
-                SELECT lwl.userName,
-                    lwl.listName,
-                    COUNT(*) as likeCount
-                FROM ListsWithLikes lwl
-                GROUP BY lwl.userName,
-                    lwl.listName
-            ),
-            TotalLikesByItem AS (
-                SELECT lwi.itemID,
-                    lwi.itemType,
-                    CAST(SUM(llc.likeCount) AS INTEGER) as totalLikes
-                FROM ListsWithItemIDs lwi
-                    JOIN ListLikeCounts llc ON lwi.userName = llc.userName
-                    AND lwi.listName = llc.listName
-                GROUP BY lwi.itemID,
-                    lwi.itemType
+        match list_type{
+            ListType::Anime => sqlx::query_as!(
+                ListOrderedByLikes,
+                r#"WITH ListsWithItemIDs AS (
+                    SELECT l.userName,
+                        l.listName,
+                        li.itemID,
+                        l.listType as itemType
+                    FROM ListItems li
+                        JOIN Lists l ON li.userName = l.userName
+                        AND li.listName = l.listName
+                ),
+                ListsWithLikes AS (
+                    SELECT l.*
+                    FROM Lists l
+                        JOIN Likes lk on l.listName = lk.listName
+                ),
+                ListLikeCounts AS (
+                    SELECT lwl.userName,
+                        lwl.listName,
+                        COUNT(*) as likeCount
+                    FROM ListsWithLikes lwl
+                    GROUP BY lwl.userName,
+                        lwl.listName
+                ),
+                TotalLikesByItem AS (
+                    SELECT lwi.itemID,
+                        lwi.itemType,
+                        CAST(SUM(llc.likeCount) AS INTEGER) as totalLikes
+                    FROM ListsWithItemIDs lwi
+                        JOIN ListLikeCounts llc ON lwi.userName = llc.userName
+                        AND lwi.listName = llc.listName
+                    GROUP BY lwi.itemID,
+                        lwi.itemType
+                )
+                SELECT id,
+                    title,
+                    mediaimage,
+                    createdon,
+                    type AS "type: ListType",
+                    totalLikes
+                FROM Media m
+                    JOIN TotalLikesByItem l ON m.id = l.itemID
+                    AND m.type = l.itemType
+                WHERE m.type = 'anime'
+                ORDER BY l.totalLikes DESC
+            "#
             )
-            SELECT id,
-                title,
-                mediaimage,
-                createdon,
-                type AS "type: ListType",
-                totalLikes
-            FROM Media m
-                JOIN TotalLikesByItem l ON m.id = l.itemID
-                AND m.type = l.itemType
-            WHERE m.type = $1
-            ORDER BY l.totalLikes DESC
-        "#,
-            list_type
-        )
-        .fetch_all(pool)
-        .await
-        .map(|a| {
-            a.into_iter()
-                .map(|a| List {
-                    username: a.username,
-                    listname: a.listname,
-                    listtype: a.listtype,
-                })
-                .collect::<Vec<List>>()
-        })
-        .map_err(|e| ErrorList(format!("failed to retrieve explore lists: {e:#?}")))
-    }
+             
+            .fetch_all(pool)
+            .await
+            .map(|a| {
+                a.into_iter()
+                    .map(|a| List {
+                        username: a.username,
+                        listname: a.listname,
+                        listtype: a.listtype,
+                    })
+                    .collect::<Vec<List>>()
+            })
+            .map_err(|e| ErrorList(format!("failed to retrieve explore lists: {e:#?}")))
+            },
+            ListType::Movies => sqlx::query_as!(
+                ListOrderedByLikes,
+                r#"WITH ListsWithItemIDs AS (
+                    SELECT l.userName,
+                        l.listName,
+                        li.itemID,
+                        l.listType as itemType
+                    FROM ListItems li
+                        JOIN Lists l ON li.userName = l.userName
+                        AND li.listName = l.listName
+                ),
+                ListsWithLikes AS (
+                    SELECT l.*
+                    FROM Lists l
+                        JOIN Likes lk on l.listName = lk.listName
+                ),
+                ListLikeCounts AS (
+                    SELECT lwl.userName,
+                        lwl.listName,
+                        COUNT(*) as likeCount
+                    FROM ListsWithLikes lwl
+                    GROUP BY lwl.userName,
+                        lwl.listName
+                ),
+                TotalLikesByItem AS (
+                    SELECT lwi.itemID,
+                        lwi.itemType,
+                        CAST(SUM(llc.likeCount) AS INTEGER) as totalLikes
+                    FROM ListsWithItemIDs lwi
+                        JOIN ListLikeCounts llc ON lwi.userName = llc.userName
+                        AND lwi.listName = llc.listName
+                    GROUP BY lwi.itemID,
+                        lwi.itemType
+                )
+                SELECT id,
+                    title,
+                    mediaimage,
+                    createdon,
+                    type AS "type: ListType",
+                    totalLikes
+                FROM Media m
+                    JOIN TotalLikesByItem l ON m.id = l.itemID
+                    AND m.type = l.itemType
+                WHERE m.type = 'movies'
+                ORDER BY l.totalLikes DESC
+            "#
+            ),
+            ListType::Songs => sqlx::query_as!(
+                ListOrderedByLikes,
+                r#"WITH ListsWithItemIDs AS (
+                    SELECT l.userName,
+                        l.listName,
+                        li.itemID,
+                        l.listType as itemType
+                    FROM ListItems li
+                        JOIN Lists l ON li.userName = l.userName
+                        AND li.listName = l.listName
+                ),
+                ListsWithLikes AS (
+                    SELECT l.*
+                    FROM Lists l
+                        JOIN Likes lk on l.listName = lk.listName
+                ),
+                ListLikeCounts AS (
+                    SELECT lwl.userName,
+                        lwl.listName,
+                        COUNT(*) as likeCount
+                    FROM ListsWithLikes lwl
+                    GROUP BY lwl.userName,
+                        lwl.listName
+                ),
+                TotalLikesByItem AS (
+                    SELECT lwi.itemID,
+                        lwi.itemType,
+                        CAST(SUM(llc.likeCount) AS INTEGER) as totalLikes
+                    FROM ListsWithItemIDs lwi
+                        JOIN ListLikeCounts llc ON lwi.userName = llc.userName
+                        AND lwi.listName = llc.listName
+                    GROUP BY lwi.itemID,
+                        lwi.itemType
+                )
+                SELECT id,
+                    title,
+                    mediaimage,
+                    createdon,
+                    type AS "type: ListType",
+                    totalLikes
+                FROM Media m
+                    JOIN TotalLikesByItem l ON m.id = l.itemID
+                    AND m.type = l.itemType
+                WHERE m.type = 'songs'
+                ORDER BY l.totalLikes DESC
+            "#
+            ),
+            ListType::VideoGames => sqlx::query_as!(
+                ListOrderedByLikes,
+                r#"WITH ListsWithItemIDs AS (
+                    SELECT l.userName,
+                        l.listName,
+                        li.itemID,
+                        l.listType as itemType
+                    FROM ListItems li
+                        JOIN Lists l ON li.userName = l.userName
+                        AND li.listName = l.listName
+                ),
+                ListsWithLikes AS (
+                    SELECT l.*
+                    FROM Lists l
+                        JOIN Likes lk on l.listName = lk.listName
+                ),
+                ListLikeCounts AS (
+                    SELECT lwl.userName,
+                        lwl.listName,
+                        COUNT(*) as likeCount
+                    FROM ListsWithLikes lwl
+                    GROUP BY lwl.userName,
+                        lwl.listName
+                ),
+                TotalLikesByItem AS (
+                    SELECT lwi.itemID,
+                        lwi.itemType,
+                        CAST(SUM(llc.likeCount) AS INTEGER) as totalLikes
+                    FROM ListsWithItemIDs lwi
+                        JOIN ListLikeCounts llc ON lwi.userName = llc.userName
+                        AND lwi.listName = llc.listName
+                    GROUP BY lwi.itemID,
+                        lwi.itemType
+                )
+                SELECT id,
+                    title,
+                    mediaimage,
+                    createdon,
+                    type AS "type: ListType",
+                    totalLikes
+                FROM Media m
+                    JOIN TotalLikesByItem l ON m.id = l.itemID
+                    AND m.type = l.itemType
+                WHERE m.type = 'videogames'
+                ORDER BY l.totalLikes DESC
+            "#
+            ),
+        }
 
     pub async fn get_top_lists(
         pool: &sqlx::PgPool,
